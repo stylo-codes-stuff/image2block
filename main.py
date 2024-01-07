5932cbcbe439a51b404d8520f6950068cf00c872
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-01-07 01:12:34 +0000

    added in the ability to upload images to the website

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from PIL import Image
+import os
+import glob
 app = Flask(__name__)
+ 
+upload_folder = os.path.join('static', 'uploads')
+ 
+app.config['UPLOAD'] = upload_folder
+ 
+@app.route('/', methods=['GET', 'POST'])#https://geekpython.in/render-images-from-flask
+def upload_file():
+    for file in os.listdir("static/uploads"):
+        os.remove(file)
+    if request.method == 'POST':
 
-
-@app.route('/', methods=['GET',"POST"])
-def index():
-    return render_template("base.html",title="test")
-
-
-app.run(host='0.0.0.0', port=0)
+        file = request.files['img']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD'], filename))
+        img = os.path.join(app.config['UPLOAD'], filename)
+        image = Image.open(img).convert('RGB')
+        image = image.resize((500, 500))
+        image.save(os.path.join(app.config['UPLOAD'], filename))
+        return render_template('base.html', img=img)
+    return render_template('base.html')
+ 
+ 
+if __name__ == '__main__':
+    app.run(debug=True, port=8001)

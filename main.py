5b2ab76551ec24a6b591390168789dab200fe68f
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-01-07 21:04:28 +0000

    slimmed down the main file alot by loading images as b64 strings

--- main.py
+++ main.py
@@ -6,17 +6,14 @@ import base64
 app = Flask(__name__)
  
 upload_folder = os.path.join('static', 'uploads')
- 
-app.config['UPLOAD'] = upload_folder
- 
+  
 @app.route('/', methods=['GET', 'POST'])#https://geekpython.in/render-images-from-flask
 def homepage():
-    return render_template("base.html")
-@app.route('/result', methods=['POST'])
-def upload_file():
-    file = request.files['img']
-    image_string = base64.b64encode(file.read())
-    return render_template('base.html', img = image_string)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    if request.method == 'POST':
+        file = request.files['img']
+        image_string = base64.b64encode(file.read())
+        return render_template('base.html', img = image_string)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    return render_template('base.html')
 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)

c7209122cf98013f76a540d8b4d3bd13cdcb2863
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-01-07 20:20:31 +0000

    managed to allow image upload without secure filepath also added in error pages

---
+++ createpack.py
@@ -0,0 +1,6 @@
+import os
+'''these functions are solely for creating the datapacks and sending them'''
+
+'''creates the datapack to be sent to the client'''
+def create_pack(image_name):
+    pass
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 from PIL import Image
 import os
-import glob
+import base64
 app = Flask(__name__)
  
 upload_folder = os.path.join('static', 'uploads')
@@ -10,21 +10,13 @@ upload_folder = os.path.join('static', 'uploads')
 app.config['UPLOAD'] = upload_folder
  
 @app.route('/', methods=['GET', 'POST'])#https://geekpython.in/render-images-from-flask
+def homepage():
+    return render_template("base.html")
+@app.route('/result', methods=['POST'])
 def upload_file():
-    for file in os.listdir("static/uploads"):
-        os.remove(file)
-    if request.method == 'POST':
+    file = request.files['img']
+    image_string = base64.b64encode(file.read())
+    return render_template('base.html', img = image_string)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
 
-        file = request.files['img']
-        filename = secure_filename(file.filename)
-        file.save(os.path.join(app.config['UPLOAD'], filename))
-        img = os.path.join(app.config['UPLOAD'], filename)
-        image = Image.open(img).convert('RGB')
-        image = image.resize((500, 500))
-        image.save(os.path.join(app.config['UPLOAD'], filename))
-        return render_template('base.html', img=img)
-    return render_template('base.html')
- 
- 
 if __name__ == '__main__':
     app.run(debug=True, port=8001)

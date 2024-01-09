6d059e587404ca24180bc4c69de25f2644dafe1f
Stylo <136652260+stylo-codes-stuff@users.noreply.github.com>
2024-01-09 16:28:00 +0000

    began writing datapack creation functions that use tempfile

--- createpack.py
+++ createpack.py
@@ -1,6 +1,14 @@
 import os
+import tempfile as tp   
+import shutil
+from PIL import Image
 '''these functions are solely for creating the datapacks and sending them'''
+fp = tp.TemporaryFile()
 
 '''creates the datapack to be sent to the client'''
-def create_pack(image_name):
-    pass
+def create_pack(image_name=""):#https://stackoverflow.com/questions/18550127/how-to-do-virtual-file-processing
+    with tp.TemporaryDirectory() as f:
+        shutil.copy(f.name, 'new-name')
+        print('created temporary directory', f)
+create_pack()
+print(fp)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request
-from werkzeug.utils import secure_filename
 from PIL import Image
 import os
 import base64
+from io import StringIO,BytesIO
 app = Flask(__name__)
  
 upload_folder = os.path.join('static', 'uploads')
@@ -12,8 +12,11 @@ def homepage():
     if request.method == 'POST':
         file = request.files['img']
         image_string = base64.b64encode(file.read())
-        return render_template('base.html', img = image_string)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+        img = Image.open(BytesIO(base64.b64decode(image_string)))
+
+        return render_template('base.html', img = image_string)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
     return render_template('base.html')
 
-if __name__ == '__main__':
+if __name__ == '__main__':    
     app.run(debug=True, port=8001)
